7575be94bde2f716166de1137ab01d74af021012
floaaat <142103054+floaaat@users.noreply.github.com>
2024-11-03 00:45:44 +0300

    just many changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,10 @@
 if status is-interactive
     alias hx='helix'
-    alias ls='eza -alh --icons=always --no-filesize -s type'
-    alias lss='eza -alh --icons=always --no-time --total-size -rs size'
-    alias tree='eza -a --icons=always --sort=type -TL'
+    alias ls='eza -a -s type -w 60 --icons=always'
     alias grep='grep --color=auto'
-    alias ipkgs='pacman -Qqe | fzf --layout=reverse --border=rounded --margin=1,2 \
-    --color=bw --prompt=i: --bind="enter:execute(pacman -Qi {} | less)"'
-    alias apkgs='pacman -Sql | fzf --layout=reverse --border=rounded --margin=1,2 \
-    --color=bw --prompt=a: --bind="enter:execute(pacman -Si {} | less)"'
-
-    starship init fish | source
-    enable_transience
-
+    alias ipkgs='pacman -Qqe | fzf --layout=reverse --color=bw --prompt=i: \
+    --bind="enter:execute(pacman -Qi {} | less)"'
+    alias apkgs='pacman -Sql | fzf --layout=reverse --color=bw --prompt=a: \
+    --bind="enter:execute(pacman -Si {} | less)"'
     set -U fish_greeting
-
-    fastfetch
 end
